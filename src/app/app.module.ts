f774ae23127642bfac804297490aa67b50687855
Shaikh Alamara <alamara786shaikh@gamail.com>
2026-08-11 16:03:01 +0530

    Fixed CSS advance directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BasicCssDirective } from './shared/directives/basicCss.directive';
+import { CssAdvanceDirective } from './shared/directives/css-advance.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicCssDirective
+    BasicCssDirective,
+    CssAdvanceDirective
      
 
   ],
--- src/app/shared/directives/css-advance.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CssAdvanceDirective } from './css-advance.directive';
-
-describe('CssAdvanceDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CssAdvanceDirective();
-    expect(directive).toBeTruthy();
-  });
-});
